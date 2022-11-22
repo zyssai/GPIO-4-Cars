ce7c2c63aca71929d188812c8c3b036fe4b8ee2b
zyssai <jc.richter@free.fr>
2022-11-22 17:22:14 +0100

    WIP

--- GPIO_4_Cars.ino
+++ GPIO_4_Cars.ino
@@ -17,11 +17,16 @@ const int pin_input_5v = A1;
 const int pin_input_12v = 1;
 const int pin_n2_power = 4;
 const int pin_pwm_output = 0;
-const int analog_value = 800;
+
+const int analog_5v_value = 800;
+const int boot_time_duration = 8000;
 int input_pwm = 0;
 int input_5v = 0;
+int boot_time = 0;
+bool boot_sequence = true;
 bool input_12v = false;
 bool power_output = false;
+bool old_power_output = false;
  
 void setup() {
   pinMode(pin_input_pwm, INPUT);
@@ -39,21 +44,35 @@ void loop() {
 void power() {    // Set n2_power pin HIGH if input_12v OR input_5v are HIGH
   input_5v = analogRead(pin_input_5v);
   input_12v = digitalRead(pin_input_12v);
-  if ((input_12v = true) || (input_5v > analog_value)) {
+  if ((input_12v = true) || (input_5v > analog_5_value)) {
     power_output = true;
-    analogWrite(pin_pwm_output, 38);
-    digitalWrite(pin_n2_power, HIGH);
-  }
-  else{
+  } else {
     power_output = false;
-    delay(1000);
-    digitalWrite(pin_n2_power, LOW);
+  }
+  if (power_output != old_power_output) {
+    if (power_output = true) {
+      boot_sequence = true;
+      digitalWrite(pin_n2_power, HIGH);  
+    } else {
+      delay(1000);
+      digitalWrite(pin_n2_power, LOW);
+    }
+  old_power_output = power_output;
   }
 }
 
 void pwm() {    // Set PWM
-  if (power_output = true) {
+  if (boot_sequence = false) {
     input_pwm = analogRead(pin_input_pwm);
     analogWrite(pin_pwm_output, pin_input_pwm);
+  } else {
+    if (boot_time >= 0) {
+      analogWrite(pin_pwm_output, 38);
+      boot_time = (boot_time - 100);
+      if (boot_time <= 0) {
+        boot_time = 8000;
+        boot_sequence = false;
+      }
+    }
   }
 }
